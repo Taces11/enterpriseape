06ce40c22632f6aa2ffa64438acaf2c08a17fd62
Taces11 <terryaces@gmail.com>
2014-09-14 13:47:49 -0400

    'added a dynamic form dropdown'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140914132718) do
+ActiveRecord::Schema.define(version: 20140914170348) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -22,13 +22,21 @@ ActiveRecord::Schema.define(version: 20140914132718) do
     t.datetime "updated_at"
   end
 
+  create_table "employees", force: true do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.string   "job_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "invoices", force: true do |t|
     t.datetime "date"
     t.string   "company"
     t.decimal  "tax"
-    t.string   "salesperson"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "employee_id"
     t.string   "status_type"
   end
 
